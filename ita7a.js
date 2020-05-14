5f229efc79850575a8f9e86f5365c8bb0441e3fc
ita7a <57022144+ita7a@users.noreply.github.com>
2020-05-14 20:17:04 +0300

    Create ita7a.js

---
+++ ita7a.js
@@ -0,0 +1,29 @@
+$ ( ` <center> <div style =" width: 100٪؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample">- ¦¦ - شات التحليه - ¦¦ -</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+$(`<link rel="stylesheet" type="text/css" href="https://rawcdn.githack.com/ita7a/ita7a.coml/3cc316c016350db618845eea56a7bf14ed007277/ita7a.css">
+<link rel="stylesheet"href="href="//www.fontstatic.com/f=jazeera-light,jazeera">
+</style>`).insertBefore('body');
+$(".label-primary").css("border-radius","10px 10px 10px 10px")
+$(".tbox").attr('maxlength',150); 
+AddMsg_=AddMsg;
+AddMsg=function(wid,data){data.msg=data.msg.replace(/\u102C/g,"");return AddMsg_(wid,data);}
+$(".nav-tabs").addClass("fl").css("width","100%");var _0xb650=["maxlength","100","attr","textarea#tbox.fl.corner.tbox.filw"];
+if(location.protocol!='https:'){location.protocol='https:';}
+$("[data-target=#wall]").attr('title','- ¦¦ - مبدعيـن التحليـه - ¦¦ -')
+$(`<img tabindex="0" class="al120 fl " style="margin-left: -4px; padding:5px; width:40px;margin-top: -4px;" src="/sico/1558730372080.png" data-original-title="" title="">`).insertAfter('img.fl.nosel.emobox');
+$(".al120").click(function(){
+alert("عذرا لا تملك الصلاحية للدخول  ");
+});
+$(".al121").click(function(){
+alert("عذرا لا تملك صلاحيات");
+});
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(` <a class="fl mini fa fa-bullhorn  btn btnn-default">&nbsp;ألأستماع لـ اذاعه شات التحليه</a><input type="jfa-w" name="FirstName" value="تحليتنا اكيد احلى"><audio src="http://philae.shoutca.st:8114/;stream.mp3" controls loop></audio></div><center>`).appendTo("#settings"),clearInterval(b)):console.log(c)},1)});  
+if(location.protocol!='https:') {location.protocol='https:';}
+if (getUrlParameter('enter') != null) {location.href='/';} 
+$('.break.light').css({"background-image":"url()"});
+$.getScript("");
+$(`<img style="height:50px;width: 97%;margin-right: 3px;background-color: #736D5D;" src="/sico/1585244292040.gif" border="5">`).insertBefore('div#d2bc');
+
+$("#d0 .fa-gear").html('&nbsp;').css('width','36px');
+
+$(`<font size="4" color="#FFFFFF"><marquee direction="right" width="100%" height="20" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5" style="height: 20px; width: 100%;background-color:#961340;">أهلا وسهلا بكم في شات التحليه الرسمي : تستطيع الدخول من جوالك او اللاب توب او الاي باد ومن جميع الاجهزة اللوحيه نتمنى لكم قضاء اجمل الاوقات معنـــا
+</marquee></font>`).insertBefore('.nav-tabs'); 
